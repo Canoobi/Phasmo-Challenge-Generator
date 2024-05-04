eb25f86499869c0c5628f533180dfd04a14f20bf
Canoob <105295932+Canoobi@users.noreply.github.com>
2024-05-04 19:13:26 +0200

    update TODOs

--- src/ChallengeRad.java
+++ src/ChallengeRad.java
@@ -2,6 +2,9 @@ import javax.swing.*;
 
 public class ChallengeRad {
     public static void main(String[] args) {
+
+        //TODO add new json files to readme
+
         SwingUtilities.invokeLater(() -> {
             SelectChallenge wheelGUI = new SelectChallenge();
             wheelGUI.setVisible(true);
