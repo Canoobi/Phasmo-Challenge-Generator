f0e048dc17599674f9d4d4822a823f9b12f3e8a1
Canoobi <carlo.gliech@gmx.de>
2024-07-23 20:08:51 +0200

    move main-class

--- src/main/java/com/files/ChallengeRad.java
+++ src/main/java/com/files/ChallengeRad.java
@@ -1,8 +1,11 @@
+package com.files;
+
 import javax.swing.*;
 
 public class ChallengeRad {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
+            System.out.println("Hello World!");
             SelectChallenge wheelGUI = new SelectChallenge();
             wheelGUI.setVisible(true);
         });
